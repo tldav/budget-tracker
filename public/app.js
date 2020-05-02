0ee47a96241efff7bc08a263c0d0e9d5f5ee15f0
Neverender <tldavis09@yahoo.com>
2020-05-01 20:48:14 -0500

    idk how but I fixed the service worker issue where it wasn't activating. No idea what I changed. Now I just need to get the cache to work

--- public/app.js
+++ public/app.js
@@ -2,8 +2,6 @@ if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
 		navigator.serviceWorker.register("sw.js").then((reg) => {
 			console.log("service worker registered", reg);
-		}).catch((err) => {
-            console.log(err);
-        }
+		});
 	});
 }
